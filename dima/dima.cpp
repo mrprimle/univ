a6a5fe9f5dfdae0d586791e6cade60934c3951e2
Mike D <30389608+mrprimle@users.noreply.github.com>
2021-03-09 23:36:18 +0200

    lab2

--- dima/dima.cpp
+++ dima/dima.cpp
@@ -1,39 +1,183 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TLinktwo
+{
+    int data;
+    TLinktwo* left;
+    TLinktwo* right;
+};
 
 
-
-
-void writeEmp(int emp) {
-    FILE* fpointer = fopen_s("emp.txt", "a");
-    for (int i = 0; i < emp; i++) {
-        printf("Enter first name: ");
-        char name1[20];
-        scanf("%s", name1);
-        fprintf(fpointer, name1);
-
-        printf("Enter last name: ");
-        char name2[20];
-        scanf_s("%s", name2);
-        fprintf(fpointer, " %s \n", name2);
-
-        for (int j = 1; j < 8; j++) {
-            int det;
-            printf("Enter number of details made in %d day of the week: ", j);
-            scanf_s("%d", &det);
-            fprintf(fpointer, "%d ", det);
-
-        }
+void add_element(TLinktwo*& head, TLinktwo*& tail, int data, int place)
+{
+    TLinktwo* temp = new TLinktwo;
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    if (head == NULL)// is_empty
+    {
+        tail = temp;
+        head = tail;
+        return;
     }
-
-    fclose(fpointer);
+    if (place == 1) // add_right
+    {
+        head->right = temp;
+        temp->left = head;
+        head = temp;
+    }
+    else // add_left
+    {
+        tail->left = temp;
+        temp->right = tail;
+        tail = temp;
+    }
 }
 
+bool remove_element(TLinktwo*& head, TLinktwo*& tail, int& data, int place)
+{
+    TLinktwo* temp = new TLinktwo;
+    if (head == NULL)// is_empty
+    {
+        return false;
+    }
+    if (head == tail) // one element
+    {
+        data = head->data;
+        delete head;
+        delete tail;
+        head = NULL;
+        tail = NULL;
+        return true;
+    }
+    if (place == 1) // remove_right
+    {
+        data = head->data;
+        temp = head;
+        head = head->left;
+        delete temp;
+        temp = NULL;
+        head->right = NULL;
+    }
+    else // remove_left
+    {
+        data = tail->data;
+        temp = tail;
+        tail = tail->right;
+        delete temp;
+        temp = NULL;
+        tail->left = NULL;
+    }
+    return true;
+}
+
+bool view_element(TLinktwo*& head, TLinktwo*& tail, int place)
+{
+    TLinktwo* temp = new TLinktwo;
+    temp = tail;
+    if (head == NULL)
+    {
+        cout << "Empty\n";
+        return false;
+    }
+    if (place == 3) cout << head->data << "\n"; // view_right
+    else cout << tail->data << "\n"; // view_left
+    return true;
+}
+
+
+bool view_all(TLinktwo*& head, TLinktwo*& tail)
+{
+    TLinktwo* temp = new TLinktwo;
+    temp = tail;
+    if (head == NULL)
+    {
+        cout << "Empty\n";
+        return false;
+    }
+    while (temp != head)
+    {
+        cout << temp->data << " ";
+        temp = temp->right;
+    }
+    cout << temp->data << "\n";
+    return true;
+}
+
+void interface_display()
+{
+    cout << "What do u need?\n";
+    cout << "[ADD_R] ";
+    cout << "[ADD_L] ";
+    cout << "[REM_R] ";
+    cout << "[REM_L] ";
+    cout << "[VIEW_R] ";
+    cout << "[VIEW_L] ";
+    cout << "[ALL] ";
+    cout << "[EXIT]\n";
+}
+
+int listener()
+{
+    string input;
+    cin >> input;
+    if (input == "ADD_R") return 1;
+    if (input == "ADD_L") return 2;
+    if (input == "REM_R") return -1;
+    if (input == "REM_L") return -2;
+    if (input == "VIEW_R") return 3;
+    if (input == "VIEW_L") return 4;
+    if (input == "EXIT") return 0;
+    else return 9;
+}
+
+void interface(TLinktwo* head, TLinktwo* tail)
+{
+    bool execute = true;
+    int data, ch;
+    while (execute)
+    {
+        interface_display();
+        ch = listener();
+        if (ch == 0) execute = false;
+        else
+        {
+            if (ch == 1 || ch == 2)
+            {
+                cout << "DATA = ";
+                cin >> data;
+                add_element(head, tail, data, ch);
+            }
+            else
+            {
+                if (ch == 9)
+                {
+                    view_all(head, tail);
+                }
+                else
+                {
+                    if (ch == 3 || ch == 4)
+                    {
+                        view_element(head, tail, ch);
+                    }
+                    else
+                    {
+                        ch = abs(ch);
+                        if (remove_element(head, tail, data, ch)) cout << "DATA: " << data << "\n";
+                        else cout << "Empty\n";
+                    }
+                }
+            }
+        }
+    }
+}
 
 int main()
 {
-    writeEmp(2);
-
+    TLinktwo* head = NULL;
+    TLinktwo* tail = NULL;
+    interface(head, tail);
     return 0;
 }
-
---
+++ lab2sem2/lab2sem2.cpp
@@ -0,0 +1,157 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+struct Node {
+    vector<string> data;
+    struct Node* next;
+};
+
+
+
+
+
+
+string multiply(string x, string y) {
+    string res = "00000000";
+    for (int i = 0; i < x.size(); i++) {
+        if (x[i] == '1' && y[i] == '1') {
+            res[i] = '1';
+        }
+        else {
+            res[i] = '0';
+        }
+    }
+    return res;
+}
+
+
+string pluss(string x, string y) {
+    string res = "00000000";
+    for (int i = 0; i < x.size(); i++) {
+        if (x[i] == '1' || y[i] == '1') {
+            res[i] = '1';
+        }
+        else {
+            res[i] = '0';
+        }
+    }
+    return res;
+}
+
+
+string arrayPlus(vector<string> arr) {
+    if (arr.size() == 1) {
+        return arr[0];
+    }
+    else {
+        for (int i = 0; i < arr.size() - 1; i++) {
+            string vec = pluss(arr[i], arr[i + 1]);
+            arr[i + 1] = vec;
+
+        }
+        return arr[arr.size() - 1];
+    }
+
+
+
+}
+
+
+
+
+string arrayMul(vector<string> arr) {
+    if (arr.size() == 1) {
+        return arr[0];
+    }
+    else {
+        for (int i = 0; i < arr.size() - 1; i++) {
+            string vec = multiply(arr[i], arr[i + 1]);
+            arr[i + 1] = vec;
+
+        }
+        return arr[arr.size() - 1];
+    }
+
+
+
+}
+
+
+
+
+vector<string> normArray(vector<string> arr) {
+    vector<string> res = {};
+    for (auto elem : arr) {
+        if (elem.size() == 1) {
+            if (elem == "x") {
+                res.push_back("00001111");
+            }
+            else if (elem == "y") {
+                res.push_back("00110011");
+            }
+            else {
+                res.push_back("01010101");
+            }
+        }
+        else {
+            if (elem[1] == 'x') {
+                res.push_back("11110000");
+            }
+            else if (elem[1] == 'y') {
+                res.push_back("11001100");
+            }
+            else {
+                res.push_back("10101010");
+            }
+        }
+    }
+    return res;
+}
+
+
+
+string traverseLL(struct Node* n)
+{
+    vector<string> itemsToPlus = {};
+    while (n != NULL) {
+        itemsToPlus.push_back(arrayMul(normArray(n->data)));
+        n = n->next;
+    }
+    return arrayPlus(itemsToPlus);
+}
+
+int main()
+{
+    
+
+    // allocate 3 nodes in the heap 
+    Node * head = new Node;
+    Node* second = new Node;
+    Node* third = new Node;
+    
+
+    head->data = {"x", "y", "z"}; // assign data in first node 
+    head->next = second; // Link first node with second 
+
+    second->data = { "x", "~y"}; // assign data to second node 
+    second->next = third;
+
+    third->data = {"z"}; // assign data to third node 
+    third->next = NULL;
+
+
+
+
+
+
+
+    cout << traverseLL(head);
+
+    return 0;
+}
